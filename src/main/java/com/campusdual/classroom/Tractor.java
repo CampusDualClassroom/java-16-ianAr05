e4ed34e045423c08f05bfc0e0fd3ed8cca404085
Ian <ianagrafojo2@gmail.com>
2025-06-03 12:59:31 +0200

    16

--- src/main/java/com/campusdual/classroom/Tractor.java
+++ src/main/java/com/campusdual/classroom/Tractor.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public class Tractor{
+public class Tractor implements  IMachine{
 
 	protected int horsePower = 0;
 	
@@ -16,4 +16,21 @@ public class Tractor{
 		System.out.println("El tractor retrocede");
 	}
 
+	@Override
+	public void start() {
+		System.out.println("Esta encendido");
+
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Esta apagado");
+
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("Se ecnuentra realizando un mantenimiento");
+
+	}
 }
